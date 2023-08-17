27cab1dd984c14bd2ad199c3d57b4b2e6e9ae8a9
Александр <ignatenko.ap@yandex.ru>
2023-08-17 19:50:04 +0300

    hw_4.2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Домашнее адание 4.2");
+        System.out.println("Домашнее задание 4.2");
     }
 }
